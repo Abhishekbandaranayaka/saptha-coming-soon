09756919a8911b96c665efb848cbdfd9fabdb6e8
Abhisheksandeepa <89740367+Abhishekbandaranayaka@users.noreply.github.com>
2026-01-17 08:49:26 +0530

    actual page

---
+++ components/sub-navbar.php
@@ -0,0 +1,206 @@
+ <!-- Side Menu -->
+ <div class="side-menu" id="sideMenu">
+     <div class="overlay" id="overlay"></div>
+     <a href="javascript:void(0)" class="close-btn" id="closeBtn" aria-label="close button"><i class="fa fa-close"></i>
+         close</a>
+     <div class="menu-content">
+         <a class='logo' href='index.php' aria-label="home">
+             <img src="assets/images/logo/logo-white.png" alt="logo">
+         </a>
+         <div class="sidebar-menu">
+             <h4 class="title">contacts</h4>
+             <p>166A/A/01<br> Egaloya, Bulathsinhala</p>
+             <a href="tel:+94773340795" aria-label="mobile" class="nmbr">(+94)
+                 773340795</a>
+             <a href="https://wa.me/0760543394" aria-label="whatsapp" target="_blank" class="nmbr">(+94)
+                 760543394</a>
+             <a href="mailto:info@sapthaglobal.com" aria-label="email" class="email">info@sapthaglobal.com</a>
+         </div>
+         <ul class="social-icon">
+             <li><a href="www.facebook.html" aria-label="facebook"><i class="fab fa-facebook-f"></i></a></li>
+             <li><a href="https://x.com/i/flow/login?lang=en" aria-label="x"><i class="fab fa-twitter"></i></a></li>
+             <li><a href="https://www.linked.com/" aria-label="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+             <li><a href="https://wa.me/0760543394" aria-label="whatsapp" target="_blank"><i
+                         class="fab fa-whatsapp"></i></a></li>
+         </ul>
+     </div>
+ </div>
+ <!-- New Mobile Menu -->
+ <div data-menu="mobileMenu" class="side-menu2">
+     <div class="menu-btns">
+         <button id="mobileCloseBtn2" class="close-btn"></button>
+     </div>
+     <ul>
+         <li><a href="about.php" aria-label="about us">About Us</a></li>
+         <li><a href="careers.php" aria-label="careers">Careers</a></li>
+         <li><a href="services.php" aria-label="services">Services</a></li>
+         <li><a href="contacts.php" aria-label="contacts">Contacts</a></li>
+     </ul>
+     <div class="menu-contact">
+         <span>Contacts</span>
+         <a href="tel:+94773340795" class="nmbr" aria-label="mobile">(+94) 773340795</a>
+         <a href="mailto:info@sapthaglobal.com" aria-label="email" class="gmail">info@sapthaglobal.com</a>
+     </div>
+     <div class="menu-links">
+         <span>Follow us:</span>
+         <ul class="social-icon">
+             <li><a href="#" aria-label="facebook"><i class="fab fa-facebook-f"></i></a></li>
+             <li><a href="#" aria-label="x"><i class="fab fa-twitter"></i></a></li>
+             <li><a href="#" aria-label="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+             <li><a href="https://wa.me/0760543394" aria-label="whatsapp" target="_blank"><i
+                         class="fab fa-whatsapp"></i></a></li>
+         </ul>
+
+     </div>
+ </div>
+ <!-- Overlay for Mobile Menu -->
+ <div class="overlay2"></div>
+
+ <!-- sticky header -->
+ <header class="sticky-active">
+     <div class="header-menu-area">
+         <div class="row gx-20 align-items-center justify-content-between">
+             <div class="col-auto">
+                 <div class="header-logo">
+                     <a href="javascript:void(0)" aria-label="toggle" class="menu-toggle"></a>
+                     <a href='index.php' aria-label="home">
+                         <img src="assets/images/logo/logo.png" width="220" alt="logo">
+                     </a>
+                 </div>
+             </div>
+             <div class="col-auto">
+                 <nav class="main-menu menu-style1 v4" style="background-color: black;">
+                     <ul>
+                         <li>
+                             <a href='about.php' aria-label="about us">
+                                 <span class=" menu-item">About Us</span>
+                                 <span class="menu-item2">About Us</span>
+                             </a>
+                         </li>
+                         <li>
+                             <a href='services.php' aria-label="services">
+                                 <span class="menu-item">Services</span>
+                                 <span class="menu-item2">Services</span>
+                             </a>
+                         </li>
+                         <li>
+                             <a href='careers.php' aria-label="careers">
+                                 <span class="menu-item">Careers</span>
+                                 <span class="menu-item2">Careers</span>
+                             </a>
+                         </li>
+                         <li>
+                             <a href='contacts.php' aria-label="contacts">
+                                 <span class="menu-item">Contacts</span>
+                                 <span class="menu-item2">Contacts</span>
+                             </a>
+                         </li>
+                     </ul>
+                 </nav>
+             </div>
+             <div class="col-auto d-none d-xl-block">
+                 <div class="btn-box">
+                     <a href="contacts.php" aria-label="contacts" class="ibt-btn ibt-btn-outline-3 ibt-btn-rounded">
+                         <span>Get in Touch</span>
+                     </a>
+                 </div>
+             </div>
+         </div>
+         <button class="hamburger popup-menu" data-menu="mobileMenu">
+             <span></span>
+             <span></span>
+             <span></span>
+         </button>
+     </div>
+ </header>
+ <!-- End sticky header -->
+
+ <!--======== Header ========-->
+ <header class="vs-header15">
+     <div class="header-top4">
+         <div class="container-fluid">
+             <div class="header-top-content4">
+                 <ul class="top-bar-contacts">
+                     <li>
+                         <span class="contact-item-title">Call us:</span>
+                         <a href="tel:+94773340795" aria-label="mobile">(+94) 773340795</a>
+                     </li>
+                     <li>
+                         <span class="contact-item-title">Email:</span>
+                         <a href="mailto:info@sapthaglobal.com" aria-label="email">info@sapthaglobal.com</a>
+                     </li>
+                 </ul>
+                 <ul class="top-bar-socials">
+                     <li><span>Follow us:</span></li>
+                     <li><a href="#" aria-label="facebook"><i class="fab fa-facebook-f"></i></a></li>
+                     <li><a href="#" aria-label="x"><i class="fab fa-twitter"></i></a></li>
+                     <li><a href="#" aria-label="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                     <li><a href="https://wa.me/0760543394" aria-label="whatsapp" target="_blank"><i
+                                 class="fab fa-whatsapp"></i></a></li>
+                 </ul>
+             </div>
+         </div>
+     </div>
+     <!--vs-main-menu-wrapper start-->
+     <div class="header-bottom15">
+         <div class="container2 position-relative">
+             <div class="header-menu-area">
+                 <div class="row gx-20 align-items-center justify-content-between">
+                     <div class="col-auto">
+                         <div class="header-logo">
+                             <a href="javascript:void(0)" aria-label="toggle" class="menu-toggle"></a>
+                             <a href='index.php' aria-label="home">
+                                 <img src="assets/images/logo/logo.png" width="220" alt="logo">
+                             </a>
+                         </div>
+                     </div>
+                     <div class="col-auto">
+                         <nav class="main-menu menu-style1 v4" style="background-color: black;">
+                             <ul>
+                                 <li>
+                                     <a href='about.php' aria-label="about us">
+                                         <span class=" menu-item">About Us</span>
+                                         <span class="menu-item2">About Us</span>
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href='services.php' aria-label="services">
+                                         <span class="menu-item">Services</span>
+                                         <span class="menu-item2">Services</span>
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href='careers.php' aria-label="careers">
+                                         <span class="menu-item">Careers</span>
+                                         <span class="menu-item2">Careers</span>
+                                     </a>
+                                 </li>
+                                 <li>
+                                     <a href='contacts.php' aria-label="contacts">
+                                         <span class="menu-item">Contacts</span>
+                                         <span class="menu-item2">Contacts</span>
+                                     </a>
+                                 </li>
+                             </ul>
+                         </nav>
+                     </div>
+                     <div class="col-auto d-none d-xl-block">
+                         <div class="btn-box">
+                             <a href="contacts.php" aria-label="contacts"
+                                 class="ibt-btn ibt-btn-outline-3 ibt-btn-rounded">
+                                 <span>Get in Touch</span>
+                             </a>
+                         </div>
+                     </div>
+                 </div>
+                 <button class="hamburger popup-menu" data-menu="mobileMenu">
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                 </button>
+             </div>
+         </div>
+     </div>
+     <!--vs-main-menu-wrapper end-->
+ </header>
+ <!--======== / Header ========-->
